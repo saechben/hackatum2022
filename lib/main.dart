e959d757a0df95212fabd02b7515aa83655bb40c
JanosCsampai <janoscsampai@live.de>
2022-11-19 16:26:40 +0100

    add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,162 +1,55 @@
-import 'dart:io';
-import 'dart:async';
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/cupertino.dart';
+import 'map.dart';
 
-// fetch from localhost and creat PointOfInterest from response.body
-Future<List<PointOfInterest>> fetchPointOfInterest()async{
-  final response = await http.get(Uri.parse('http://10.0.2.2:8000/api/issues/?format=json'));
-  if(response.statusCode == 200){
-    var list = json.decode(response.body);
-    var pointOfInterestList = list.map<PointOfInterest>((json) => PointOfInterest.fromJson(json)).toList();
-    return pointOfInterestList;
-  }else{
-    throw Exception('Failed to load PointOfInterest');
-  }
-}
+void main() => runApp(const CupertinoTabBarApp());
 
-class PointOfInterest{
-  final double longitude;
-  final double latitude;
-  final int? userID;
-  final int oSMway;
-
-    const PointOfInterest({
-      required this.longitude,
-      required this.latitude,
-      required this.userID,
-      required this.oSMway,
-    });
-
-    factory PointOfInterest.fromJson(Map<String,dynamic>json){
-      print(json);
-      return PointOfInterest(
-        longitude: json['longitude'],
-        latitude: json['latitude'],
-        userID: json['solved_by_id'],
-        oSMway: json['osm_way_id'],
-      );
-    }
-}
-
-void main() {
-  runApp(const MyApp());
-  // await controller.addMarker(GeoPoint,markerIcon:MarkerIcon,angle:pi/3);
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class CupertinoTabBarApp extends StatelessWidget {
+  const CupertinoTabBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wormhole Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Wormhole Demo Home Page'),
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoTabBarExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<PointOfInterest>> futurePointofInterest;
-
-   MapController mapController = MapController(
-                            initMapWithUserPosition: true,
-                            initPosition: GeoPoint(latitude: 14.599512, longitude: 120.984222),
-                            areaLimit: const BoundingBox.world(),
-                       );
-
-  Future<int> async_sleep(time) async {
-      // sleep 1 second
-      await Future.delayed(Duration(seconds: time));
-      return time;
-    }
-
-  @override
-  initState() {
-    // super.initState();
-
-    futurePointofInterest = fetchPointOfInterest();
-    // print(futurePointofInterest);
-
-    async_sleep(1).then((unused) async {
-      // for all point of interest add marker with latitude and longitude
-      for (var pointOfInterest in await futurePointofInterest) {
-        print(pointOfInterest.latitude);
-        print(pointOfInterest.longitude);
-        mapController.addMarker(GeoPoint(latitude: pointOfInterest.latitude, longitude: pointOfInterest.longitude));
-      }
-    });
-    
-  }
+class CupertinoTabBarExample extends StatelessWidget {
+  const CupertinoTabBarExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.star_fill),
+            label: 'Favourites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.clock_solid),
+            label: 'Recents',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_alt_circle_fill),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.circle_grid_3x3_fill),
+            label: 'Keypad',
+          ),
+        ],
       ),
-      body: Stack(
-          children: <Widget>[
-            OSMFlutter( 
-              controller: mapController,
-              trackMyPosition: true,
-              initZoom: 15,
-              stepZoom: 1.0,
-              userLocationMarker: UserLocationMaker(
-                  personMarker: const MarkerIcon(
-                      icon: Icon(
-                          Icons.location_history_rounded,
-                          color: Colors.red,
-                          size: 48,
-                      ),
-                  ),
-                  directionArrowMarker: const MarkerIcon(
-                      icon: Icon(
-                          Icons.double_arrow,
-                          size: 48,
-                      ),
-                  ),
-              ),
-              roadConfiguration: RoadConfiguration(
-                      startIcon: const MarkerIcon(
-                        icon: Icon(
-                          Icons.person,
-                          size: 64,
-                          color: Colors.brown,
-                        ),
-                      ),
-                      roadColor: Colors.yellowAccent,
-              ),
-              markerOption: MarkerOption(
-                  defaultMarker: const MarkerIcon(
-                      icon: Icon(
-                        Icons.person_pin_circle,
-                        color: Colors.blue,
-                        size: 56,
-                        ),
-                      )
-              ),
-          ) 
-          ],
-        ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return const Center(
+              child: MyApp(),
+            );
+          },
+        );
+      },
     );
   }
 }
---
+++ lib/map.dart
@@ -0,0 +1,151 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:http/http.dart' as http;
+
+// fetch from localhost and creat PointOfInterest from response.body
+Future<List<PointOfInterest>> fetchPointOfInterest()async{
+  final response = await http.get(Uri.parse('http://10.0.2.2:8000/api/issues/?format=json'));
+  if(response.statusCode == 200){
+    var list = json.decode(response.body);
+    var pointOfInterestList = list.map<PointOfInterest>((json) => PointOfInterest.fromJson(json)).toList();
+    return pointOfInterestList;
+  }else{
+    throw Exception('Failed to load PointOfInterest');
+  }
+}
+
+class PointOfInterest{
+  final double longitude;
+  final double latitude;
+  final int? userID;
+  final int oSMway;
+
+    const PointOfInterest({
+      required this.longitude,
+      required this.latitude,
+      required this.userID,
+      required this.oSMway,
+    });
+
+    factory PointOfInterest.fromJson(Map<String,dynamic>json){
+      print(json);
+      return PointOfInterest(
+        longitude: json['longitude'],
+        latitude: json['latitude'],
+        userID: json['solved_by_id'],
+        oSMway: json['osm_way_id'],
+      );
+    }
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wormhole Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Wormhole Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<PointOfInterest>> futurePointofInterest;
+
+   MapController mapController = MapController(
+                            initMapWithUserPosition: true,
+                            initPosition: GeoPoint(latitude: 14.599512, longitude: 120.984222),
+                            areaLimit: const BoundingBox.world(),
+                       );
+
+  Future<int> async_sleep(time) async {
+      // sleep 1 second
+      await Future.delayed(Duration(seconds: time));
+      return time;
+    }
+
+  @override
+  initState() {
+    // super.initState();
+
+    futurePointofInterest = fetchPointOfInterest();
+    // print(futurePointofInterest);
+
+    async_sleep(1).then((unused) async {
+      // for all point of interest add marker with latitude and longitude
+      for (var pointOfInterest in await futurePointofInterest) {
+        print(pointOfInterest.latitude);
+        print(pointOfInterest.longitude);
+        mapController.addMarker(GeoPoint(latitude: pointOfInterest.latitude, longitude: pointOfInterest.longitude));
+      }
+    });
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+          children: <Widget>[
+            OSMFlutter( 
+              controller: mapController,
+              trackMyPosition: true,
+              initZoom: 15,
+              stepZoom: 1.0,
+              userLocationMarker: UserLocationMaker(
+                  personMarker: const MarkerIcon(
+                      icon: Icon(
+                          Icons.location_history_rounded,
+                          color: Colors.red,
+                          size: 48,
+                      ),
+                  ),
+                  directionArrowMarker: const MarkerIcon(
+                      icon: Icon(
+                          Icons.double_arrow,
+                          size: 48,
+                      ),
+                  ),
+              ),
+              roadConfiguration: RoadConfiguration(
+                      startIcon: const MarkerIcon(
+                        icon: Icon(
+                          Icons.person,
+                          size: 64,
+                          color: Colors.brown,
+                        ),
+                      ),
+                      roadColor: Colors.yellowAccent,
+              ),
+              markerOption: MarkerOption(
+                  defaultMarker: const MarkerIcon(
+                      icon: Icon(
+                        Icons.person_pin_circle,
+                        color: Colors.blue,
+                        size: 56,
+                        ),
+                      )
+              ),
+          ) 
+          ],
+    );
+  }
+}
